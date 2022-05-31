0f2cae7c86ca4ae033e1a621ab85d27cbbf3e0e1
samfang <samfang@gmail.com>
2022-05-31 16:43:01 -0400

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,16 +2,10 @@ module.exports = {
   dest: 'docs/dist',
   
   themeConfig: {
-    repo: 'MetaMask/metamask-docs',
     docsDir: 'docs',
     docsBranch: 'main',
     editLinks: true,
-    logo: '/metamask-fox.svg',
     smoothScroll: true,
-    algolia: {
-      apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: process.env.ALGOLIA_INDEX_NAME,
-    },
     locales: {
       '/': {
         label: 'English',
@@ -38,46 +32,6 @@ const guideSidebar = [
       'initializing-dapps',
       'accessing-accounts',
       'sending-transactions',
-    ],
-  },
-  {
-    title: 'API Reference',
-    collapsable: false,
-    children: [
-      'ethereum-provider',
-      'provider-migration',
-      'rpc-api',
-      'signing-data',
-    ],
-  },
-  {
-    title: 'Best Practices',
-    collapsable: false,
-    children: [
-      'registering-function-names',
-      'registering-your-token',
-      'defining-your-icon',
-      'onboarding-library',
-      'metamask-extension-provider',
-    ],
-  },
-  {
-    title: 'Mobile',
-    collapsable: false,
-    children: [
-      'mobile-getting-started',
-      'site-compatibility-checklist',
-      'mobile-best-practices',
-    ],
-  },
-  {
-    title: 'Snaps',
-    collapsable: false,
-    children: [
-      'snaps',
-      'snaps-development-guide',
-      'snaps-rpc-api',
-      'snaps-patching-dependencies',
     ],
   },
   {
